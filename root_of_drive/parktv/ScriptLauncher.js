4e081c885532cf296c45e7682ec748c694cc52ac
Corey Edwards <ensabahnur16@gmail.com>
2021-04-04 16:46:20 -0500

    Add backend ParkTV files
    These files needed to be added to the root of the device. These were needed for background syncing of the files for ParkTV

---
+++ root_of_drive/parktv/ScriptLauncher.js
@@ -0,0 +1,2 @@
+var wshShell = new ActiveXObject("WScript.Shell");
+wshShell.Run('%SystemRoot%\\system32\\WindowsPowerShell\\v1.0\\powershell.exe -ExecutionPolicy Bypass -File "C:\\parktv\\PTV2_BackgroundSync.ps1"', 0, false);
